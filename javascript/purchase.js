9bf3b193eacb32a0c1db4bdf118b4df253a9a2cc
Liran Ziv <liran27@gmail.com>
2017-10-29 21:30:03 +0200

    Third Commit
    Added some functionality to the purchase page.

--- javascript/purchase.js
+++ javascript/purchase.js
@@ -1,21 +1,34 @@
 
 
-var $androidQuan = $("#androidQuantity");
-var $iosQuan = $("#iosQuantity");
-$(document).ready(function (e) {
+$(function () {
 
-    $('section-shoppingCart').hide();
-    $("checkoutIOS").click(updateCartIos($iosQuan));
-    $("checkoutAndroid").click(updateCartAndroid($androidQuan));
+    $(".section-shoppingCart").hide();
+
+    $('#checkoutAndroid').click(function () {
+        var quan = $('#androidQuantity').val();
+        var totalPrice = 0;
+        if (quan > 1)
+            totalPrice = ((20 * 0.75) + (20 * (quan - 1)));
+        else if (quan == 1)
+            totalPrice = 20;
+        else
+            totalPrice = 0;
+        $(".section-shoppingCart").show();
+        $("#androidNum").attr($(quan));
+        $("#total").attr(totalPrice);
+    });
+
+
+    $('#checkoutIOS').click(updateCartIos($("#iosQuantity")));
 
     function updateCartAndroid(quantity) {
         var totalPrice = 0;
-        if (quantity > 1)
+        if (quantity > 1) {
             totalPrice = ((20 * 0.75) + (20 * (quantity - 1)));
-        else
+        }
+        else {
             totalPrice = 20;
-        $();
-
+        }
     }
     function updateCartIos(quantity) {
         var totalPrice = 0;
@@ -23,10 +36,6 @@ $(document).ready(function (e) {
             totalPrice = ((25 * 0.75) + (25 * (quantity - 1)));
         else
             totalPrice = 25;
-
-        return totalPrice;
     }
-
-
 
 });
